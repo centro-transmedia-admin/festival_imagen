d321ba1a87045c6f2f4ea3ccaa1e190b928cc3c5
diegoreal2002 <diegoreal2002@gmail.com>
2022-10-12 15:42:26 -0500

    changing images

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css'
 const Header = ({ title }) => {
     return (
         <header className={styles.sign}>
-            <img src="/rotulo.png" width="300px" />
+            <img src="/rotulo.jpg" width="300px" />
             <h3 className={styles.title}>{title}</h3>
         </header>
     )
